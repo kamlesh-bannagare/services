581e9c6dacf318b73c0633120476cc797c745d35
kamlesh bannagare <kamleshbannagare10@gmail.com>
2022-01-01 19:29:51 +0530

    contact no added in contact file

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -3,6 +3,7 @@ import "./ContactUs.css"
 const ContactUs = () => {
     return (
         <div>
+            <h3>Contact No. 7066884596</h3>
 <lable className="">name</lable>
 <input type="text"></input>
 <lable className="">contact number</lable>
